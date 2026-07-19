c5d5f4e5777d05c2169b485e2964b14d1f011136
nikhilpame33 <nikhilpame333@gmail.com>
2026-07-19 10:29:27 +0530

    issue fix in yml and loginHelper

--- tests/Assignment_1/loginHelper.spec.js
+++ tests/Assignment_1/loginHelper.spec.js
@@ -1,15 +1,14 @@
 const {test,expect} = require("@playwright/test");
-const username = "Nikhil@gmail.com"
-const password = "Nikhil@1"
 
-test("@login page", async({page, username, password}) =>
+
+test("@login page", async({page}) =>
 {
+    const username = "Nikhil@gmail.com"
+    const password = "Nikhil@1"
     await page.goto("https://eventhub.rahulshettyacademy.com")
-
     await page.getByPlaceholder('you@email.com').fill(username);
     await page.getByLabel('Password').fill(password);
     await page.locator("#login-btn").click()
     await page.locator("#nav-home").waitFor()
-    await expect (page.locator("#nav-home").isVisible())
-    
+    await expect (page.locator("#nav-home")).toBeVisible()    
 })
